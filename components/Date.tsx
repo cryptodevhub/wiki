ed6721cfab9b1daa2c45147c7638d62363f1af69
Philipp Muens <philipp@muens.io>
2022-06-24 08:59:21 +0200

    Remove Date component

--- components/Date.tsx
+++
@@ -1,6 +0,0 @@
-import { parseISO, format } from 'date-fns'
-
-export default function Date({ date }: { date: string }) {
-  const parsed = parseISO(date)
-  return <time dateTime={date}>{format(parsed, 'LLLL d, yyyy')}</time>
-}
