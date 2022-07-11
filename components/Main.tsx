057650dfeaeb8c47bc8c08347b77a80ec2bdbece
Philipp Muens <philipp@muens.io>
2022-07-11 08:02:55 +0200

    Update Main component styles

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,3 +1,3 @@
 export default function Main({ children }: { children: React.ReactNode }) {
-  return <main className="max-w-7xl w-full mx-auto pt-8 pb-24 flex-1 px-8 lg:px-4">{children}</main>
+  return <main className="container mx-auto px-5">{children}</main>
 }
