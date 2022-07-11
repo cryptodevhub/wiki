8d1cdb0858f99fc6d2b7c2f892d484094b8099cd
Philipp Muens <philipp@muens.io>
2022-07-11 08:27:54 +0200

    Update tags show page styles

--- pages/tags/[slug].tsx
+++ pages/tags/[slug].tsx
@@ -1,26 +1,28 @@
 import { NextSeo } from 'next-seo'
 import { GetStaticPaths, GetStaticProps } from 'next'
 
-import ContentCard from '../../components/Content'
+import Card from '../../components/Card'
 import { toTitleCase } from '../../lib/utils'
 import { getAllContentTags, Content, getContentByTags } from '../../lib/content'
 
 export default function Show({ slug, content }: { slug: string; content: Content[] }) {
-  const title = toTitleCase(slug)
+  const title = toTitleCase(slug).replace(/-/g, ' ')
   const description = `Content tagged with "${slug}"`
 
   return (
     <>
       <NextSeo title={title} description={description} />
-      <article>
-        <div className="text-center mb-8">
-          <h1 className="font-bold text-4xl">{title}</h1>
+      <article className="text-gray-600 body-font">
+        <div className="flex pt-12 justify-center">
+          <div className="text-center lg:w-2/3 w-full">
+            <h1 className="title-font sm:text-4xl text-3xl font-medium text-gray-900">{title}</h1>
+          </div>
         </div>
         <section>
-          <ul className="grid gap-8 mt-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <ul className="grid gap-8 pt-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {content.map((content) => (
               <li key={content.slug}>
-                <ContentCard content={content} />
+                <Card content={content} />
               </li>
             ))}
           </ul>
