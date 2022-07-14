866c96abebcda5dd3638148ea58ba766f9e0a1da
Philipp Muens <philipp@muens.io>
2022-07-14 09:49:11 +0200

    Shuffle tags array

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,10 +1,24 @@
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 import { ArrowSmRightIcon } from '@heroicons/react/outline'
 
 import { Content } from '../lib/content'
 
+// Note: We're using `useEffect` and `useState` here to enforce a client-only rendering.
+//  This is necessary because we're shuffling the `tags` array which results in hydration
+//  issues if done on the client- and server side.
+//  See the following StackOverflow answer for more information: https://stackoverflow.com/a/72008126
 export default function Card({ content }: { content: Content }) {
   const { title, description, slug, tags } = content
+  const [shuffledTags, setShuffledTags] = useState<string[]>([])
+
+  useEffect(() => {
+    if (tags?.length) {
+      // Adaption of https://stackoverflow.com/a/18650169
+      const shuffled = tags.sort(() => 0.5 - Math.random()).slice(0, 3)
+      setShuffledTags(shuffled)
+    }
+  }, [tags])
 
   return (
     <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
@@ -24,7 +38,7 @@ export default function Card({ content }: { content: Content }) {
       <div className="flex-grow">
         <p className="leading-relaxed text-base line-clamp-2">{description}</p>
         <ul>
-          {tags.slice(0, 3).map((tag) => (
+          {shuffledTags.map((tag) => (
             <li
               key={tag}
               className="text-sm mr-2 px-2 inline-flex border border-gray-400 rounded-full hover:border-gray-600 mt-2"
